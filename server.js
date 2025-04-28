c50679b6787a97cb25615051b9ae0b8b3082f0db
FDGod <polskitroll212@gmail.com>
2025-04-28 14:55:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
 const { Server } = require("socket.io");
 require("dotenv").config();
-const serverVersion = "0.6.0";
+const serverVersion = "0.6.5";
 const app = express();
 const server = http.createServer(app);
 
